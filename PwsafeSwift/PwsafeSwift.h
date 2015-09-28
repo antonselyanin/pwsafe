195d487c69947f24a0a053e349d29e35de5c33ab
Anton Selyanin <anton.selyanin@gmail.com>
2015-09-28 22:35:21 +0600

    converted to library

---
+++ PwsafeSwift/PwsafeSwift.h
@@ -0,0 +1,19 @@
+//
+//  PwsafeSwift.h
+//  PwsafeSwift
+//
+//  Created by Anton Selyanin on 28/09/15.
+//  Copyright © 2015 Anton Selyanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PwsafeSwift.
+FOUNDATION_EXPORT double PwsafeSwiftVersionNumber;
+
+//! Project version string for PwsafeSwift.
+FOUNDATION_EXPORT const unsigned char PwsafeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PwsafeSwift/PublicHeader.h>
+
+
